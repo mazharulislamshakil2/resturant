af7ff12d61ce00ef8f300de8bd4c91da021c0f5a
mazharul islam shakil <mazharulislamshakil2@gmail.com>
2021-01-13 12:07:59 +0600

    first commit

---
+++ assets/js/active.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+   $(".main-slider").owlCarousel({
+    items:1,
+    loop: false,
+    dots: true,
+    nav: true,
+    navText: ["<i class='fas fa-angle-left'></i>","<i class='fas fa-angle-right'></i>"]
+  });
+
+});
